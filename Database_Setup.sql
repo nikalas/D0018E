b1037065ce32ab7322a54b372d6bb97760d6f031
Victor Persson <vicper-4@student.ltu.se>
2016-11-10 21:00:52 +0000

    more minor sql fixes to mariadb syntax

--- Database_Setup.sql
+++ Database_Setup.sql
@@ -1,22 +1,23 @@
+DROP DATABASE D0018E_Webshop;
+
 CREATE DATABASE D0018E_Webshop;
 
 USE D0018E_Webshop;
 
 CREATE TABLE Category
 (
-	id 		smallint 	NOT NULL AUTO_INCREMENT,
-	name 	varchar 	NOT NULL,
+	id 		smallint 	AUTO_INCREMENT,
+	name 	varchar(127),
 	parent 	smallint,
-	PRIMARY KEY (id),
-	--FOREIGN KEY (parent) REFERENCES Category(id)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE Product
 (
-	id 				smallint 	NOT NULL AUTO_INCREMENT,
-	name 			varchar 	NOT NULL,
-	description 	varchar,
-	picture 		varchar,
+	id 				smallint 	AUTO_INCREMENT,
+	name 			varchar(127) 	NOT NULL,
+	description 	text,
+	picture 		varchar(127),
 	category 		smallint,
 	price 			float 		NOT NULL,
 	stock 			smallint 	NOT NULL 	DEFAULT 0,
@@ -26,10 +27,10 @@ CREATE TABLE Product
 
 CREATE TABLE Campain
 (
-	id 			smallint 	NOT NULL AUTO_INCREMENT,
-	start_date 	timestamp 	DEFAULT GETDATE(),
+	id 			smallint 	AUTO_INCREMENT,
+	start_date 	timestamp 	DEFAULT CURRENT_TIMESTAMP(),
 	end_date 	timestamp,
-	picture 	varchar,
+	picture 	varchar(127),
 	PRIMARY KEY (id)
 );
 
@@ -39,26 +40,26 @@ CREATE TABLE CampainProducts
 	product 	smallint 	NOT NULL,
 	price_mod 	smallint 	NOT NULL,
 	FOREIGN KEY (campain) REFERENCES Campain(id),
-	FOREIGN KEY (product) ERFERENCES Product(id)
+	FOREIGN KEY (product) REFERENCES Product(id)
 );
 
 CREATE TABLE Customer
 (
-	id 			smallint 	NOT NULL AUTO_INCREMENT,
-	name 		varchar,
+	id 			smallint 	AUTO_INCREMENT,
+	name 		varchar(127),
 	p_nr 		char(11),
-	adress 		varchar,
-	email 		varchar,
-	phone_nr 	varchar,
+	adress 		varchar(127),
+	email 		varchar(127),
+	phone_nr 	varchar(31),
 	password 	char(32),
-	picture 	varchar,
+	picture 	varchar(127),
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE ShoppingCart
 (
 	id 			smallint 	NOT NULL AUTO_INCREMENT,
-	products 	varchar,
+	products 	text,
 	customer 	smallint,
 	PRIMARY KEY (id),
 	FOREIGN KEY (customer) REFERENCES Customer(id)
@@ -66,15 +67,13 @@ CREATE TABLE ShoppingCart
 
 CREATE TABLE Delivery
 (
-	id 			smallint 	NOT NULL AUTO_INCREMENT,
+	id 			smallint 		NOT NULL AUTO_INCREMENT,
 	customer 	smallint,
-	adress 		varchar 	NOT NULL,
-	payed 		boolean 	NOT NULL 	DEFAULT false, 
-	shipped 	boolean 	NOT NULL 	DEFAULT false,
-	--collinummer
-	--tracking link
-	cart 		smallint 	NOT NULL,
-	oder_date 	date 		DEFAULT GETDATE(),
+	adress 		varchar(127) 	NOT NULL,
+	payed 		boolean 		NOT NULL 	DEFAULT false, 
+	shipped 	boolean 		NOT NULL 	DEFAULT false,
+	cart 		smallint 		NOT NULL,
+	oder_date 	date,
 	PRIMARY KEY (id),
 	FOREIGN KEY (customer) REFERENCES Customer(id),
 	FOREIGN KEY (cart) REFERENCES ShoppingCart(id)
