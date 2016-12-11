12801b4082c1533ab9c03a183f84143c1ddc851d
nikalas <nikeli-3@student.ltu.se>
2016-12-11 20:48:59 +0000

    random commit i dont even know wtf i'm doing

--- Rails_app/config/routes.rb
+++ Rails_app/config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   root 'frontend_pages#index'
 
-  get 'frontend_pages/product'
-  get '/user', to: 'frontend_pages/user'
-  get '/order', to: 'frontend_pages/order'
-  get '/cart', to: 'frontend_pages/customer_cart'
-  get '/sing_up', to: 'frontend_pages/sing_up'
-  get '/sign_in', to: 'frontend_pages/sign_in'
-  get '/index', to: 'backend_pages/index'
+  get '/product', to: 'frontend_pages#product'
+  get '/user',    to: 'frontend_pages#user'
+  get '/order',   to: 'frontend_pages#order'
+  get '/cart',    to: 'frontend_pages#customer_cart'
+  get '/sing_up', to: 'frontend_pages#sing_up'
+  get '/sign_in', to: 'frontend_pages#sign_in'
+  get '/admin',   to: 'backend_pages#index'
 
   resources :sales_products
   resources :sales
--- Rails_app/test/controllers/frontend_pages_controller_test.rb
+++ Rails_app/test/controllers/frontend_pages_controller_test.rb
@@ -7,43 +7,43 @@ class FrontendPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get frontend_pages_index_url
+    get '/index'
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get product" do
-    get frontend_pages_product_url
+    get '/product'
     assert_response :success
     assert_select "title", "Produkter | #{@base_title}"
   end
 
   test "should get user" do
-    get frontend_pages_user_url
+    get '/user'
     assert_response :success
     assert_select "title", "Användare | #{@base_title}"
   end
 
   test "should get order" do
-    get frontend_pages_order_url
+    get '/order'
     assert_response :success
     assert_select "title", "Order | #{@base_title}"
   end
 
   test "should get customer_cart" do
-    get frontend_pages_customer_cart_url
+    get '/cart'
     assert_response :success
     assert_select "title", "Kundkorg | #{@base_title}"
   end
 
   test "should get sing_up" do
-    get frontend_pages_sing_up_url
+    get '/sign_up'
     assert_response :success
     assert_select "title", "registrera | #{@base_title}"
   end
 
   test "should get sign_in" do
-    get frontend_pages_sign_in_url
+    get '/sign_in'
     assert_response :success
     assert_select "title", "Sign in | #{@base_title}"
   end
