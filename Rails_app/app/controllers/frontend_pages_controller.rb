12df88a25caaed9bd581cc82599c292a6efb734c
nikalas <nikeli-3@student.ltu.se>
2016-12-20 23:44:38 +0000

    code cleanup

--- Rails_app/app/controllers/frontend_pages_controller.rb
+++ Rails_app/app/controllers/frontend_pages_controller.rb
@@ -1,10 +1,15 @@
 class FrontendPagesController < ApplicationController
   def index
 	  @products = Product.all
+	  @categorys = Category.all
   end
 
   def product
     @product = Product.find(params[:id])
+  end
+
+  def category
+    @category = Category.find(params[:id])
   end
 
   def user
