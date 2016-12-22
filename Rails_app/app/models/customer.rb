d576b75419666ccf6117dda050543f9841927ff6
Victor Persson <vicper-4@student.ltu.se>
2016-12-22 14:58:21 +0000

    Add some constraints on users names, emails and Pnums. Some basic tests for these aswell.

--- Rails_app/app/models/customer.rb
+++ Rails_app/app/models/customer.rb
@@ -1,2 +1,12 @@
 class Customer < ApplicationRecord
+  validates :name,  presence: true, length: { maximum: 127 }
+
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
+  validates :email, presence: true, length: { maximum: 127 },
+                    format: { with: EMAIL_REGEX }
+
+  P_NR_REGEX = /\A\d{6}[-+]\d{4}\z/
+  validates :p_nr, presence: true, format: { with: P_NR_REGEX }
+
+  has_secure_password
 end
--- Rails_app/test/models/customer_test.rb
+++ Rails_app/test/models/customer_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = Customer.new(name: "Example User", p_nr: "740101-1111", email: "user@example.com", password: "asdf")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = ""
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
 end
