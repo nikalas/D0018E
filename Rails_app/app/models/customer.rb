be1922b4e6a2471d81b72db844bfac884e853039
Victor Persson <vicper-4@student.ltu.se>
2016-12-22 17:03:53 +0000

    some small additions to the Customer model and tests. The tests are broken after bcrypt replaced the password in the database table. Might be a issue with rails or with spacing in YAML. It is not necessary that they work for now.

--- Rails_app/app/models/customer.rb
+++ Rails_app/app/models/customer.rb
@@ -1,12 +1,19 @@
 class Customer < ApplicationRecord
+  #TODO Should emails be stored as lowercase?
+  #before_save { self.email = email.downcase }
+	
   validates :name,  presence: true, length: { maximum: 127 }
 
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
   validates :email, presence: true, length: { maximum: 127 },
-                    format: { with: EMAIL_REGEX }
+                    format: { with: EMAIL_REGEX }, 
+					uniqueness: { case_sensitive: false }
 
   P_NR_REGEX = /\A\d{6}[-+]\d{4}\z/
   validates :p_nr, presence: true, format: { with: P_NR_REGEX }
 
   has_secure_password
+
+  #TODO Decide minimum password requirements
+  #validates :password, presence: true, length: { minimum: 6 }  
 end
--- Rails_app/test/models/customer_test.rb
+++ Rails_app/test/models/customer_test.rb
@@ -27,4 +27,29 @@ class CustomerTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
 end
