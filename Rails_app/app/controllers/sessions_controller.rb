e9667059a205f06b29710eb5c0fce2eb0f8e27f5
Victor Persson <vicper-4@student.ltu.se>
2016-12-23 12:56:19 +0000

    fix indentation

--- Rails_app/app/controllers/sessions_controller.rb
+++ Rails_app/app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = Customer.find_by(email: params[:session][:email])
-  if user && user.authenticate(params[:session][:password])
+  	if user && user.authenticate(params[:session][:password])
       log_in user
       redirect_to root_path, :flash => {:success => "Welcome!"}
     else
