dbff88289c1663377c2e5f6c3ebdee00e8cd206e
Victor Persson <vicper-4@student.ltu.se>
2016-12-23 18:10:25 +0100

    Cleaned up and added some password checks for customer creation. Still does not check the password validation. Also made emails be stored as lowercase.

--- Rails_app/app/models/customer.rb
+++ Rails_app/app/models/customer.rb
@@ -1,19 +1,25 @@
 class Customer < ApplicationRecord
-  #TODO Should emails be stored as lowercase?
-  #before_save { self.email = email.downcase }
-	
-  validates :name,  presence: true, length: { maximum: 127 }
+  validates :name,  presence: true, 
+                    length: { maximum: 127 }
 
+  before_save { self.email = email.downcase }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i  
-  validates :email, presence: true, length: { maximum: 127 },
+  validates :email, presence: true, 
+                    length: { maximum: 127 },
                     format: { with: EMAIL_REGEX }, 
-					uniqueness: { case_sensitive: false }
+                    uniqueness: { case_sensitive: false }
 
   P_NR_REGEX = /\A\d{6}[-+]\d{4}\z/
-  validates :p_nr, format: { with: P_NR_REGEX }, allow_blank: true
+  validates :p_nr,  format: { with: P_NR_REGEX }, 
+                    allow_blank: true
 
   has_secure_password
+  validates :password,  confirmation: true,
+                        :presence => { :on => :create },
+                        length: { minimum: 6 }
 
-  #TODO Decide minimum password requirements
-  #validates :password, presence: true, length: { minimum: 6 }  
+  # TODO check that password is the same as password_confirmation. This should
+  # work automaticly, but does not. 
+#  validates_confirmation_of :password
+#  validates :password_confirmation, length: { minimum: 6 }
 end
