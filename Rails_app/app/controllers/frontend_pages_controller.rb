37d1fab21983b3e27d7c96cacf255e59a8210ca7
nikalas <nikeli-3@student.ltu.se>
2016-12-24 00:08:30 +0000

    factor out product-grid to partial

--- Rails_app/app/controllers/frontend_pages_controller.rb
+++ Rails_app/app/controllers/frontend_pages_controller.rb
@@ -2,6 +2,11 @@ class FrontendPagesController < ApplicationController
   def index
 	  @products = Product.all
 	  @categorys = Category.all
+
+	  # These two rows are for selecting what products to render
+	  # in the product grid.
+	  number_of_products = 6
+    @products_to_render = @products.limit(number_of_products).order("RANDOM()")
   end
 
   def product
