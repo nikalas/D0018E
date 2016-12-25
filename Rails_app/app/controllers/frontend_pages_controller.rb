b8355accac7c4487b6240a6b92365d974c1adc18
nikalas <nikeli-3@student.ltu.se>
2016-12-25 16:54:01 +0000

    Use category names instead of category id's

--- Rails_app/app/controllers/frontend_pages_controller.rb
+++ Rails_app/app/controllers/frontend_pages_controller.rb
@@ -1,12 +1,10 @@
 class FrontendPagesController < ApplicationController
   def index
-	  @products = Product.all
-	  @categorys = Category.all
 
 	  # These two rows are for selecting what products to render
 	  # in the product grid.
 	  number_of_products = 6
-    @products_to_render = @products.limit(number_of_products).order("RANDOM()")
+    @products_to_render = Product.limit(number_of_products).order("RANDOM()")
   end
 
   def product
@@ -15,6 +13,7 @@ class FrontendPagesController < ApplicationController
 
   def category
     @category = Category.find(params[:id])
+    @products_to_render = Product.where(category_id: @category.id)
   end
 
   def user
