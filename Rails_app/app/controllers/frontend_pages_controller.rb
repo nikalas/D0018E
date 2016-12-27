ee19be84011b86b48189801032bc45f282b0a271
Victor Persson <vicper-4@student.ltu.se>
2016-12-27 19:52:21 +0100

    Chang ordering option of products from sqlite3 function RANDOM() to mysql2 functionRAND(). This should realy be done by the sqlite/mysql gems

--- Rails_app/app/controllers/frontend_pages_controller.rb
+++ Rails_app/app/controllers/frontend_pages_controller.rb
@@ -4,7 +4,7 @@ class FrontendPagesController < ApplicationController
 	  # These two rows are for selecting what products to render
 	  # in the product grid.
 	  number_of_products = 6
-    @products_to_render = Product.limit(number_of_products).order("RANDOM()")
+    @products_to_render = Product.limit(number_of_products).order("RAND()")
   end
 
   def product
