f4696e2ea9699cd78187dd7755efd081e4952269
Victor Persson <vicper-4@student.ltu.se>
2016-12-27 17:58:10 +0100

    Made some changes to DB setup script

--- Database_Setup.sql
+++ Database_Setup.sql
@@ -10,17 +10,17 @@ CREATE TABLE schema_migrations
     PRIMARY KEY (version)
 );
 
-CREATE TABLE ar_internal_metadata (
-    key         varchar     NOT NULL, 
-    value       varchar,
+/*CREATE TABLE ar_internal_metadata (
+    key         varchar(127)     NOT NULL, 
+    value       varchar(127),
     created_at  datetime    NOT NULL,
     updated_at  datetime    NOT NULL, 
     PRIMARY KEY (key)
-);
+);*/
 
 CREATE TABLE Categories (
     id          INTEGER     NOT NULL    AUTO_INCREMENT, 
-    name        varchar, 
+    name        varchar(127), 
     parent      integer, 
     created_at  datetime    NOT NULL, 
     updated_at  datetime    NOT NULL, 
@@ -29,23 +29,23 @@ CREATE TABLE Categories (
 
 CREATE TABLE Products (
     id          INTEGER     NOT NULL    AUTO_INCREMENT, 
-    name        varchar     NOT NULL, 
+    name        varchar(127)     NOT NULL, 
     description text, 
-    picture     varchar, 
+    picture     varchar(127), 
     category_id integer,
     price       float       NOT NULL, 
     stock       integer     NOT NULL    DEFAULT 0, 
     created_at  datetime    NOT NULL, 
     updated_at  datetime    NOT NULL, 
     PRIMARY KEY (id),
-	FOREIGN KEY (category) REFERENCES Categories(id)
+	FOREIGN KEY (category_id) REFERENCES Categories(id)
 );
 
 CREATE TABLE Sales (
     id          INTEGER     NOT NULL AUTO_INCREMENT, 
     start_date  datetime    DEFAULT CURRENT_TIMESTAMP(), 
     end_date    datetime, 
-    picture     varchar, 
+    picture     varchar(127), 
     created_at  datetime    NOT NULL, 
     updated_at  datetime    NOT NULL,
     PRIMARY KEY (id)
@@ -59,21 +59,21 @@ CREATE TABLE Sales_products (
     updated_at  datetime    NOT NULL,
     price_mod   float, 
     PRIMARY KEY (id),
-    FOREIGN KEY (campain) REFERENCES Sales(id),
-	FOREIGN KEY (product) REFERENCES Products(id)
+    FOREIGN KEY (sale_id) REFERENCES Sales(id),
+	FOREIGN KEY (product_id) REFERENCES Products(id)
 );
 
 CREATE TABLE Customers (
     id              INTEGER     NOT NULL AUTO_INCREMENT, 
-    name            varchar, 
+    name            varchar(127), 
     p_nr            char(11), 
-    adress          varchar, 
+    adress          varchar(127), 
     zip             integer, 
-    city            varchar, 
-    email           varchar, 
-    phone_nr        varchar, 
-    password_digest varchar, 
-    picture         varchar, 
+    city            varchar(127), 
+    email           varchar(127), 
+    phone_nr        varchar(127), 
+    password_digest varchar(127), 
+    picture         varchar(127), 
     created_at      datetime    NOT NULL, 
     updated_at      datetime    NOT NULL,
 	PRIMARY KEY (id)
@@ -86,24 +86,23 @@ CREATE TABLE Carts (
     created_at  datetime    NOT NULL,
     updated_at  datetime    NOT NULL,
     PRIMARY KEY (id),
-	FOREIGN KEY (customer) REFERENCES Customers(id)
+	FOREIGN KEY (customer_id) REFERENCES Customers(id)
 );
 
 CREATE TABLE Deliveries (
     id          INTEGER     NOT NULL AUTO_INCREMENT, 
     customer_id integer, 
     cart_id     integer     NOT NULL, 
-    adress      varchar     NOT NULL, 
+    adress      varchar(127)     NOT NULL, 
     zip         integer     NOT NULL, 
-    city        varchar     NOT NULL, 
+    city        varchar(127)     NOT NULL, 
     payed       boolean     NOT NULL    DEFAULT false, 
     shipped     boolean     NOT NULL    DEFAULT false, 
     order_date  date, 
     created_at  datetime    NOT NULL, 
     updated_at  datetime    NOT NULL,
     PRIMARY KEY (id),
-	FOREIGN KEY (customer) REFERENCES Customers(id),
+	FOREIGN KEY (customer_id) REFERENCES Customers(id),
 	FOREIGN KEY (cart_id) REFERENCES Carts(id)
 
 );
-
