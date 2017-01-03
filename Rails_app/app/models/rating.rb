ed1565d3810980d50f6ed2337c0d25e7f6264cd1
nikalas <nikeli-3@student.ltu.se>
2017-01-03 01:37:52 +0000

    validate that user actually submits a score

--- Rails_app/app/models/rating.rb
+++ Rails_app/app/models/rating.rb
@@ -1,5 +1,5 @@
 class Rating < ApplicationRecord
   belongs_to :product
   belongs_to :customer
-  validates :grade, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0}
+  validates :grade, presence: true, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0}
 end
