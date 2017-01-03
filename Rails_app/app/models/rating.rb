93143cf9b8d3f2bde4a547cf5c8e6e8490de690d
nikalas <nikeli-3@student.ltu.se>
2017-01-03 01:35:09 +0000

    Dont force user to give rating of at least one

--- Rails_app/app/models/rating.rb
+++ Rails_app/app/models/rating.rb
@@ -1,5 +1,5 @@
 class Rating < ApplicationRecord
   belongs_to :product
   belongs_to :customer
-  validates :grade, numericality: { less_than_or_equal_to: 5, greater_than: 0}
+  validates :grade, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0}
 end
